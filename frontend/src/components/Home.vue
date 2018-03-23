d9e8a6d05384a68027321e5fa50c416acfb02cab
akeshavan <keshavan@berkeley.edu>
2018-03-23 11:25:35 -0400

    enh: nhw thingy

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,21 +1,158 @@
 
 <template>
-  <div>
-    <p>Home page</p>
-    <p>Random number from backend: {{ randomNumber }}</p>
-    <button @click="getRandom">New random number</button>
+  <div class="content">
+    <b-container>
+      <!-- Content here -->
+      <b-row class="text-left">
+          <b-col class="left">
+            <h4>Completed: {{nComplete}}</h4>
+            <div v-for="(o, index) in order">
+              <a :class="{'name selected complete': index==startIdx && completed[index], 'name complete': index != startIdx && completed[index], 'name selected incomplete': index==startIdx && !completed[index], 'name incomplete': index != startIdx && !completed[index]}"
+                 @click="setIdx(index)">
+                 {{o.Name}}
+              </a>
+            </div>
+          </b-col>
+          <b-col cols="8" class="right">
+            <b-row>
+              <iframe class="pdfView" :src="currentPDF"></iframe>
+            </b-row>
+            <b-row class="mt-3 pt-3">
+              <b-col>
+                <h6>Computing</h6>
+                <vue-slider @callback="save" v-model="currentApplicant.Computing" tooltipDir="bottom" :width="200" :min="0" :max="10" :piecewise="true"></vue-slider>
+              </b-col>
+
+              <b-col>
+                Neuroscience
+                <vue-slider @callback="save" v-model="currentApplicant.Neuroscience" tooltipDir="bottom" :width="200" :min="0" :max="10" :piecewise="true"></vue-slider>
+              </b-col>
+
+              <b-col>
+                Statement
+                <vue-slider @callback="save" v-model="currentApplicant.Statement" tooltipDir="bottom" :width="200" :min="0" :max="10" :piecewise="true"></vue-slider>
+              </b-col>
+            </b-row>
+
+          </b-col>
+      </b-row>
+
+    </b-container>
+
   </div>
 </template>
 
+<style>
+
+  .name {
+    cursor: pointer;
+    padding: 2px;
+  }
+
+  .selected {
+    background-color: #007bff;
+    color: white !important;
+  }
+
+  .selected.completed {
+    color: white !important;
+  }
+
+  .incomplete {
+    color: #dc3545 !important;
+  }
+
+  .content, html, body {
+      height: 90%;
+  }
+  .left {
+      float: left;
+      width: 50%;
+      padding: 5px;
+      height: calc(100vh - 80px);
+      max-height: calc(100vh - 80px);
+      overflow: scroll;
+  }
+  .right {
+      float: left;
+      width: 50%;
+      height: calc(100vh - 80px);
+      max-height: calc(100vh - 80px);
+      overflow: scroll;
+  }
+  .pdfView {
+    height: 75VH;
+    width: 100%;
+  }
+</style>
+
 <script>
 import axios from 'axios'
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import vueSlider from 'vue-slider-component'
+
+Vue.use(BootstrapVue)
+Vue.use(vueSlider)
+
 export default {
   data () {
     return {
-      randomNumber: 0
+      randomNumber: 0,
+      order: [],
+      startIdx: 0
+    }
+  },
+  components: {vueSlider},
+  computed: {
+    currentApplicant () {
+      if (this.order.length) {
+        return this.order[this.startIdx]
+      } else {
+        return {}
+      }
+    },
+    currentPDF () {
+      if (this.currentApplicant) {
+        return `http://localhost:5000/api/pdf/${this.currentApplicant.Name}`
+      } else {
+        return null
+      }
+    },
+    completed () {
+      const completed = []
+      this.order.forEach((val) => {
+        if (val.Computing !== 0 && val.Neuroscience !== 0 && val.Statement !== 0) {
+          completed.push(true)
+        } else {
+          completed.push(false)
+        }
+      })
+      return completed
+    },
+    nComplete () {
+      let n = 0
+      this.completed.forEach((v) => {
+        if (v) {
+          n += 1
+        }
+      })
+      return n
     }
   },
   methods: {
+    save () {
+      axios.post('http://localhost:5000/api/save', this.order).then(() => {
+        console.log('success')
+      }).catch((e) => {
+        console.log('error', e)
+      })
+    },
+    setIdx (index) {
+      this.startIdx = index
+    },
     getRandomInt (min, max) {
       min = Math.ceil(min)
       max = Math.floor(max)
@@ -29,7 +166,9 @@ export default {
       const path = `http://localhost:5000/api/random`
       axios.get(path)
       .then(response => {
+        console.log(response)
         this.randomNumber = response.data.randomNumber
+        this.order = response.data.order
       })
       .catch(error => {
         console.log(error)
--- run.py
+++ run.py
@@ -1,8 +1,18 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, send_file, request
 from random import *
 from flask_cors import CORS
 import requests
+import sys
+import zipfile
+from glob import glob
+import os
+from os.path import exists
+import pandas as pd
+import simplejson as json
 
+applicant_df = None
+applicant_order = None
+order_file = None
 app = Flask(__name__,
             static_folder = "./dist/static",
             template_folder = "./dist")
@@ -10,10 +20,38 @@ cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 @app.route('/api/random')
 def random_number():
+    assert(applicant_df is not None)
+    assert(order_file is not None)
+
+    if (exists(order_file.replace('xlsx', 'csv'))):
+        applicant_order = pd.read_csv(order_file.replace('xlsx', 'csv'),
+        index_col=0)
+    else:
+        applicant_order = pd.read_excel(order_file)
+
+    applicant_order.fillna(0, inplace=True)
+    print(applicant_order.head())
+
     response = {
-        'randomNumber': randint(1, 100)
+        'randomNumber': randint(1, 100),
+        'order': applicant_order.to_dict(orient='record'),
     }
     return jsonify(response)
+
+@app.route('/api/pdf/<username>')
+def get_pdf(username):
+    pdf_path = applicant_df.loc[username, 0]
+    return send_file(pdf_path)
+
+@app.route('/api/save', methods=['POST'])
+def save_scores():
+    data = json.loads(request.data)  # data is empty
+    if (order_file is not None):
+        applicant_order = pd.DataFrame(data)
+        print(applicant_order.head())
+        applicant_order.to_csv(order_file.replace('xlsx', 'csv'))
+        return jsonify({'success': 1})
+    return jsonify({'success': 0})
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
@@ -21,3 +59,35 @@ def catch_all(path):
     if app.debug:
         return requests.get('http://localhost:8080/{}'.format(path)).text
     return render_template("index.html")
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        order_file = sys.argv[1]
+        zip_file = sys.argv[2]
+        # make sure the files are right
+        assert(order_file.endswith('.xlsx'))
+        assert(zip_file.endswith('.zip'))
+
+        # extract the zip file w/ all the pdfs
+        zip_ref = zipfile.ZipFile(zip_file, 'r')
+        zip_ref.extractall('/tmp/applicants')
+        zip_ref.close()
+
+        # get all the relevant files and names
+        all_applicants_path = sorted(glob('/tmp/applicants/*/*'))
+        all_applicants_names = [a.split('/')[-1] for a in all_applicants_path]
+        applicant_pdfs = [os.path.join(all_applicants_path[i], '{}.pdf'.format(a)) for i, a in enumerate(all_applicants_names)]
+        #_ = [assert(exists(a)) for a in applicant_pdfs]
+        applicant_df = pd.DataFrame(index=all_applicants_names,
+                                    data=applicant_pdfs)
+        if (exists(order_file.replace('xlsx', 'csv'))):
+            applicant_order = pd.read_csv(order_file.replace('xlsx', 'csv'),
+            index_col=0)
+        else:
+            applicant_order = pd.read_excel(order_file)
+
+        applicant_order.fillna(0, inplace=True)
+
+        app.run('0.0.0.0', '5000')
+    else:
+        raise Exception('python run.py order.xlsx zipFile.zip')
